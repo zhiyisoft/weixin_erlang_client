97b2017a051dbb683e916f1bfb550a7bdacd0be7
suyu <suyu@zhiyisoft.com>
2014-09-03 16:04:56 +0800

    weixin module

--- src/weixin_sup.erl
+++ src/weixin_sup.erl
@@ -10,8 +10,11 @@ start_link({AppId, AppSecret}) ->
   supervisor:start_link({local, ?SERVER}, ?MODULE, [AppId, AppSecret]).
 
 init([AppId, AppSecret]) ->
-  Token = {weixin_token, {weixin_token, start_link, [AppId, AppSecret]},
-           permanent, brutal_kill, worker, [weixin_token]},
-  Children = [Token],
+  Token  = {weixin_token, {weixin_token, start_link, [AppId, AppSecret]},
+            permanent, brutal_kill, worker, [weixin_token]},
+  Client = {weixin, {weixin, start_link, []},
+            permanent, brutal_kill, worker, [weixin]},
+
+  Children = [Token, Client],
   RestartStrategy = {one_for_one, 10, 3},
   {ok, {RestartStrategy, Children}}.
